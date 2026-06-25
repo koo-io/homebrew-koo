7c6165412e770e894b445b31e0f4dcc2d707dbc7
Hakan <karadumanhakan1@gmail.com>
2026-06-25 14:09:58 -0700

    Update koo to version 0.1.6

--- Formula/koo.rb
+++ Formula/koo.rb
@@ -1,18 +1,18 @@
 class Koo < Formula
   desc "Deploy to Koo from your terminal"
   homepage "https://koo.io"
-  version "0.1.0"
+  version "0.1.6"
   license "MIT"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://downloads.koo.io/koo/v0.1.0/koo-aarch64-unknown-linux-gnu.tar.gz",
-          verified: "downloads.koo.io/"
-      sha256 "1111111111111111111111111111111111111111111111111111111111111111"
+      url "https://downloads.koo0.io/koo/v0.1.6/koo-aarch64-unknown-linux-gnu.tar.gz",
+          verified: "downloads.koo0.io/"
+      sha256 "8a8a29679754b6d2359f602194953fad0d13da1ada46ab38923b5acc80a383b3"
     else
-      url "https://downloads.koo.io/koo/v0.1.0/koo-x86_64-unknown-linux-gnu.tar.gz",
-          verified: "downloads.koo.io/"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://downloads.koo0.io/koo/v0.1.6/koo-x86_64-unknown-linux-gnu.tar.gz",
+          verified: "downloads.koo0.io/"
+      sha256 "df623856d59667e96c96186bb483de6e720e74d119368182db8e70814cf90fb6"
     end
   end
 
